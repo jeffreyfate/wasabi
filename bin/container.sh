99520566b71e931e7203f35786c54ac978d4b52f
Jeffrey Fate <jeffreyfate@wisercare.com>
2019-05-01 13:07:14 -0700

    chore(config): Use port 80

--- bin/container.sh
+++ bin/container.sh
@@ -178,9 +178,9 @@ start_wasabi() {
   wenv="WASABI_CONFIGURATION=-DnodeHosts=${wcip} -Ddatabase.url.host=${wmip}"
 
 #   fixme: try to reuse the start_container() method instead of 'docker run...' directly; currently a problem with quotes in ${wenv} being passed into container.
-  docker run --net=${docker_network} --name ${project}-main -p 8080:8080 -p 8090:8090 -p 8180:8180 \
+  docker run --net=${docker_network} --name ${project}-main -p 80:8080 -p 8090:8090 -p 8180:8180 \
     -e "${wenv}" -d ${project}-main || \
-    usage "docker run --net=${docker_network} --name ${project}-main -p 8080:8080 -p 8090:8090 -p 8180:8180 -e \"${wenv}\" -d ${project}-main" 1
+    usage "docker run --net=${docker_network} --name ${project}-main -p 80:8080 -p 8090:8090 -p 8180:8180 -e \"${wenv}\" -d ${project}-main" 1
 
   echo -ne "${green}chill'ax ${reset}"
 
